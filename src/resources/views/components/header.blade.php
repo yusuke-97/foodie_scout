5c91eee0f3abc13df52443aaf1d2f6e5797936b4
yusuke-97 <tigersyuu7@gmail.com>
2023-09-15 18:33:03 +0900

    ヘッダーの作成

---
+++ src/resources/views/components/header.blade.php
@@ -0,0 +1,55 @@
+<nav class="navbar navbar-expand-md navbar-light shadow-sm header-container">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a>
+
+        @auth
+        <form class="row g-1">
+            <div class="col-auto">
+                <input class="form-control header-search-input">
+            </div>
+            <div class="col-auto">
+                <button type="submit" class="btn header-search-button"><i class="fas fa-search header-search-icon"></i></button>
+            </div>
+        </form>
+        @endauth
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ms-auto mr-5 mt-2">
+                <!-- Authentication Links -->
+                @guest
+                <li class="nav-item mr-5">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+                <li class="nav-item mr-5">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+
+                @else
+                <li class="nav-item mr-5">
+                    <a class="nav-link" href="{{ route('login') }}"><i class="far fa-heart"></i></a>
+                </li>
+                <li class="nav-item mr-5">
+                    <a class="nav-link" href="{{ route('login') }}"><i class="fas fa-bookmark"></i></a>
+                </li>
+                <hr>
+                <li class="nav-item mr-5" style="display: flex; align-items: center;">
+                    <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        ログアウト
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
