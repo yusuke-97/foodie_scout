ccce5b733d16e3b6dd7e4eebc973b4a39c8e6f46
yusuke-97 <tigersyuu7@gmail.com>
2023-09-15 15:03:51 +0900

    パスワード再設定ページの実装

--- src/resources/views/auth/passwords/email.blade.php
+++ src/resources/views/auth/passwords/email.blade.php
@@ -3,44 +3,42 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+        <div class="col-md-5">
+            <h3 class="mt-3 mb-3">パスワード再設定</h3>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+            <p>
+                ご登録時のメールアドレスを入力してください。<br>
+                パスワード再発行用のメールをお送りします。
+            </p>
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+            <hr>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+            @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
             </div>
+            @endif
+
+
+            <form method="POST" action="{{ route('password.email') }}">
+                @csrf
+
+                <div class="form-group">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror login-input" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="メールアドレス">
+
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>メールアドレスが正しくない可能性があります。</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn submit-button w-100">
+                        送信
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
