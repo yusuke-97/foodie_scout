adc4dd09307bcc33581316eb15433d2b9b89594d
yusuke-97 <tigersyuu7@gmail.com>
2023-09-18 15:16:26 +0900

    飲食店モデルとコントローラーの作成

--- src/app/Models/Restaurant.php
+++ src/app/Models/Restaurant.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Restaurant extends Model
 {
     use HasFactory;
 
-    public function restaurants()
+    public function category()
     {
-        return $this->hasMany('App\Models\Restaurant');
+        return $this->belongsTo('App\Models\Category');
     }
 }
