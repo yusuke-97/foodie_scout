e7031d3b892401a2e493635ff7efdb4339eb7175
yusuke-97 <tigersyuu7@gmail.com>
2023-09-19 17:11:04 +0900

    Vue3の環境構築

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
--- src/vite.config.js
+++ src/vite.config.js
@@ -3,6 +3,13 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: true,
+        port: 3000, 
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
         laravel({
             input: [
