d05e1abb4cda3cee10224def91616e711d3accbb
yusuke-97 <tigersyuu7@gmail.com>
2023-09-19 11:34:46 +0900

    店舗編集ページの修正

--- src/resources/views/restaurants/edit.blade.php
+++ src/resources/views/restaurants/edit.blade.php
@@ -1,56 +1,55 @@
-<div>
-    <h2>Edit Restaurant</h2>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>商品情報更新</h1>
+
+    <form action="{{ route('restaurants.update',$restaurant->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <div class="form-group">
+            <label for="restaurant-name">店舗名</label>
+            <input type="text" name="name" id="restaurant-name" class="form-control" value="{{ $restaurant->name }}">
+        </div>
+        <div class="form-group">
+            <label for="restaurant-description">説明</label>
+            <textarea name="description" id="restaurant-description" class="form-control">{{ $restaurant->description }}</textarea>
+        </div>
+        <div class="form-group">
+            <label for="restaurant-price">1人当たりの価格</label>
+            <input type="number" name="price" id="restaurant-price" class="form-control" value="{{ $restaurant->price }}">
+        </div>
+        <div class="form-group">
+            <label for="restaurant-seat">座席数</label>
+            <input type="number" name="seat" id="restaurant-seat" class="form-control" value="{{ $restaurant->seat }}">
+        </div>
+        <div class="form-group">
+            <label for="restaurant-postcode">郵便番号</label>
+            <input type="text" name="postcode" id="restaurant-postcode" class="form-control" value="{{ $restaurant->postcode }}">
+        </div>
+        <div class="form-group">
+            <label for="restaurant-address">住所</label>
+            <input type="text" name="address" id="restaurant-address" class="form-control" value="{{ $restaurant->address }}">
+        </div>
+        <div class="form-group">
+            <label for="restaurant-phone">電話番号</label>
+            <input type="text" name="phone_number" id="restaurant-phone" class="form-control" value="{{ $restaurant->phone_number }}">
+        </div>
+        <div class="form-group">
+            <label for="restaurant-category">ジャンル</label>
+            <select name="category_id" class="form-control" id="restaurant-category">
+                @foreach ($categories as $category)
+                @if ($category->id == $restaurant->category_id)
+                <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
+                @else
+                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                @endif
+                @endforeach
+            </select>
+        </div>
+        <button type="submit" class="btn btn-danger">更新</button>
+    </form>
+
+    <a href="{{ route('restaurants.index') }}">商品一覧に戻る</a>
 </div>
-<div>
-    <a href="{{ route('restaurants.index') }}"> Back</a>
-</div>
-
-<form action="{{ route('restaurants.update',$restaurant->id) }}" method="POST">
-    @csrf
-    @method('PUT')
-
-    <div>
-        <strong>店舗名</strong>
-        <input type="text" name="name" value="{{ $restaurant->name }}" placeholder="Name">
-    </div>
-    <div>
-        <strong>説明</strong>
-        <textarea style="height:150px" name="description" placeholder="description">{{ $restaurant->description }}</textarea>
-    </div>
-    <div>
-        <strong>1人当たりの価格</strong>
-        <input type="number" name="price" value="{{ $restaurant->price }}">
-    </div>
-    <div>
-        <strong>座席数</strong>
-        <input type="number" name="seat" value="{{ $restaurant->seat }}" placeholder="Seat">
-    </div>
-    <div>
-        <strong>郵便番号</strong>
-        <input type="text" name="postcode" value="{{ $restaurant->postcode }}" placeholder="Postcode">
-    </div>
-    <div>
-        <strong>住所</strong>
-        <input type="text" name="address" value="{{ $restaurant->address }}" placeholder="Address">
-    </div>
-    <div>
-        <strong>電話番号</strong>
-        <input type="text" name="phone_number" value="{{ $restaurant->phone_number }}" placeholder="Phone Number">
-    </div>
-    <div>
-        <strong>ジャンル</strong>
-        <select name="category_id">
-            @foreach ($categories as $category)
-            @if ($category->id == $restaurant->category_id)
-            <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
-            @else
-            <option value="{{ $category->id }}">{{ $category->name }}</option>
-            @endif
-            @endforeach
-        </select>
-    </div>
-    <div>
-        <button type="submit">Submit</button>
-    </div>
-
-</form>
+@endsection
