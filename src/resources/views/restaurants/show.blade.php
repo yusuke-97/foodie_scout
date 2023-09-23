13bec35b5e9da8b08f053edeb7cf01ad612fbc66
yusuke-97 <tigersyuu7@gmail.com>
2023-09-23 13:33:04 +0900

    店舗詳細画面の不要なリンクの削除

--- src/resources/views/restaurants/show.blade.php
+++ src/resources/views/restaurants/show.blade.php
@@ -30,17 +30,6 @@
                 <div class="row">
                     <div class="col-6">
                         <favorite-button :is-favorited="{{ $restaurant->isFavoritedBy(Auth::user()) ? 'true' : 'false' }}" :restaurant-id="{{ $restaurant->id }}"></favorite-button>
-                        <!-- @if($restaurant->isFavoritedBy(Auth::user()))
-                        <a href="{{ route('restaurants.favorite', $restaurant) }}" class="btn favorite-button text-favorite w-100">
-                            <i class="fa fa-heart"></i>
-                            お気に入り解除
-                        </a>
-                        @else
-                        <a href="{{ route('restaurants.favorite', $restaurant) }}" class="btn favorite-button text-favorite w-100">
-                            <i class="fa fa-heart"></i>
-                            お気に入り
-                        </a>
-                        @endif -->
                     </div>
                     <div class="col-6">
                         <a href="{{ route('restaurant.reservations', ['restaurant' => $restaurant->id]) }}" class="btn reservation-button text-dark w-100">
