30d55851add86e9a92bcf53351d9efe2680b7dfc
yusuke-97 <tigersyuu7@gmail.com>
2023-10-11 19:22:53 +0900

    Reservation.vueの不要な実装の削除とコードの整理

--- src/resources/js/components/Reservation.vue
+++ src/resources/js/components/Reservation.vue
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { Calendar, DatePicker } from 'v-calendar'
 import 'v-calendar/style.css'
 
+// 定義したプロパティ
 const props = defineProps({
   restaurantId: Number,
   restaurantPhoneNumber: String,
@@ -14,35 +15,23 @@ const props = defineProps({
   restaurantClosedDay: String
 })
 
+// 今日と明日の日付
 const today = new Date()
 const tomorrow = new Date(today)
 tomorrow.setDate(tomorrow.getDate() + 1)
 
+// 初期値の設定
 const number_of_guests = ref(1)
 const visit_date = ref(tomorrow)
-
 const masks = ref({
     title: 'YYYY年 MMMM',
 })
 
 // 休業日の曜日を取得
 const closedDay = parseInt(props.restaurantClosedDay)
-const daysUntilClosedDay = (closedDay - today.getDay() + 7) % 7 || 7
-const nextClosedDay = new Date(today)
-nextClosedDay.setDate(today.getDate() + daysUntilClosedDay)
-
-// 次の土曜日の日付を取得
-const daysUntilSaturday = (6 - today.getDay() + 7) % 7 || 7
-const nextSaturday = new Date(today)
-nextSaturday.setDate(today.getDate() + daysUntilSaturday)
-
-// 次の日曜日の日付を取得
-const daysUntilSunday = (7 - today.getDay() + 7) % 7 || 7
-const nextSunday = new Date(today)
-nextSunday.setDate(today.getDate() + daysUntilSunday)
-
 const GOOGLE_API_KEY = 'AIzaSyB4nWI9Eagle-87B_5k0DCzxlu26C1r2Iw'
 
+// 日本の祝日を取得
 async function fetchJapaneseHolidays(year) {
   const url = `https://www.googleapis.com/calendar/v3/calendars/ja.japanese%23holiday%40group.v.calendar.google.com/events?key=${GOOGLE_API_KEY}`
   try {
@@ -54,9 +43,9 @@ async function fetchJapaneseHolidays(year) {
   }
 }
 
+// 祝日と休業日のカレンダー表示設定
 onMounted(async () => {
   const holidays = await fetchJapaneseHolidays(today.getFullYear())
-
   const futureHolidays = holidays.filter(date => new Date(date) >= today)
   for (let futureHoliday in futureHolidays) {
     const date = new Date(futureHoliday)
@@ -77,6 +66,7 @@ onMounted(async () => {
   }
 })
 
+// カレンダー属性（過去の日付）
 const attributes = ref([
   {
     key: 'past-date',
@@ -85,38 +75,226 @@ const attributes = ref([
       end: today,
     }
   },
-  {
-    key: 'sunday',
-    content: { color: 'red' }, 
-    dates: {
-      start: nextSunday,
-      repeat: {
-        weekdays: 1,
-      },
-    },
-  },
-  {
-    key: 'saturday',
-    content: { color: 'blue' }, 
-    dates: {
-      start: nextSaturday,
-      repeat: {
-        weekdays: 7,
-      },
-    },
-  },
-  {
-    key: 'closed-day',
-    content: { class: 'closed-day-symbol'}, 
-    dates: {
-      start: nextClosedDay,
-      repeat: {
-        weekdays: closedDay + 1,
-      },
-    },
-  },
 ])
 
+// 予約可能な時間帯かどうかの判定
+const isReservableTime = computed(() => {
+  if (!availableTimes.value) {
+    return false
+  } else {
+    return availableTimes.value.includes(visit_time.value)
+  }
+})
+
+// ドロップダウンの幅を設定
+const selectBoxWidth = ref("")
+onMounted(() => {
+    const elementWidth = document.querySelector(".vc-container").offsetWidth
+    selectBoxWidth.value = `${elementWidth}px`
+})
+
+// 予約可能な時間帯の選択肢を計算
+const timeOptions = (() => {
+  const times = []
+  const startHour = parseInt(props.restaurantStartTime.split(":")[0])
+  const startMinutes = parseInt(props.restaurantStartTime.split(":")[1])
+  let endHour = parseInt(props.restaurantEndTime.split(":")[0])
+  const endMinutes = parseInt(props.restaurantEndTime.split(":")[1])
+
+  if (endHour >= 0 && endHour <= 2) {
+    endHour += 24
+  }
+  endHour -= 2
+
+  let currentMinute = startMinutes
+  
+  for (let i = startHour; i <= endHour; i++) {
+    if(currentMinute === 60) {
+      currentMinute = 0
+    }
+    while(currentMinute < 60) {
+      times.push(`${i.toString().padStart(2, '0')}:${currentMinute.toString().padStart(2, '0')}`)
+      if (i === endHour && currentMinute >= endMinutes) {
+        break
+      }
+      currentMinute += 30
+    }
+    currentMinute = 0
+  }
+  return times
+})()
+
+
+const visit_time = ref(timeOptions[0])
+const availableTimes = ref(null)
+let cachedVisitDate = null
+
+// 利用可能な時間帯をAPIから取得
+async function fetchAvailableTimes() {
+  try {
+    function getLocalDate(date) {
+      const year = date.getFullYear()
+      const month = (date.getMonth() + 1).toString().padStart(2, '0')
+      const day = date.getDate().toString().padStart(2, '0')
+      return `${year}-${month}-${day}`
+    }
+
+    const selectedDate = getLocalDate(new Date(visit_date.value ? visit_date.value : cachedVisitDate))
+    const response = await axios.get(`/available-seats`, {
+      params: {
+        visit_date: selectedDate,
+        start_time: props.restaurantStartTime,
+        end_time: props.restaurantEndTime,
+        restaurant_id: props.restaurantId
+      }
+    })
+    cachedVisitDate = selectedDate
+    const reservedSeatsData = response.data.reserved_seats
+
+    availableTimes.value = timeOptions.filter(time => {
+      const reservedSeats = reservedSeatsData[time] || 0
+      const availableSeats = props.restaurantSeat - reservedSeats - number_of_guests.value
+      return availableSeats >= 0
+    })
+
+  } catch (error) {
+    console.error("Error fetching available seats:", error)
+  }
+}
+
+
+const dailyAvailability = ref({})
+let year = tomorrow.getFullYear()
+let month = tomorrow.getMonth()
+
+month += 2
+if (month > 11) {
+  month -= 12
+  year++
+}
+
+const endOfMonth = new Date(year, month + 1, 1)
+const maxMonth = new Date(year, month + 1, 0)
+
+// 日別の利用可能情報をAPIから取得
+async function fetchAvailableDays() {
+  try {
+    const response = await axios.get(`/available-days`, {
+      params: {
+        start_date: tomorrow,
+        end_date: endOfMonth,
+        start_time: props.restaurantStartTime,
+        end_time: props.restaurantEndTime,
+        restaurant_id: props.restaurantId,
+        restaurant_seat: props.restaurantSeat
+      }
+    })
+
+    dailyAvailability.value = response.data
+
+    const holidays = await fetchJapaneseHolidays(today.getFullYear())
+
+    for (let formattedDate in dailyAvailability.value) {
+      const date = new Date(formattedDate)
+      const dayOfWeek = date.getDay()
+      const isHoliday = holidays.includes(formattedDate)
+    if (dayOfWeek === closedDay) {
+      attributes.value.push({
+        key: 'closed-day',
+        content: { class: 'closed-day-symbol'},
+        dates: {
+          start: formattedDate,
+          end: formattedDate
+        }
+      })
+    } else if (dailyAvailability.value[formattedDate] === true) {
+      if (dayOfWeek === 6) {
+        attributes.value.push({
+          key: 'full-saterday',
+          content: {
+            class: 'full-booked-symbol',
+            color: 'blue'
+          },
+          dates: {
+            start: formattedDate,
+            end: formattedDate
+          }
+        })
+      } else if (dayOfWeek === 0  || isHoliday === true) {
+        attributes.value.push({
+          key: 'full-sunday-holiday',
+          content: {
+            class: 'full-booked-symbol',
+            color: 'red'
+          },
+          dates: {
+            start: formattedDate,
+            end: formattedDate
+          }
+        })
+      } else {
+        attributes.value.push({
+          key: 'full',
+          content: { class: 'full-booked-symbol'},
+          dates: {
+            start: formattedDate,
+            end: formattedDate
+          }
+        })
+      }
+    } else if (dailyAvailability.value[formattedDate] === false) {
+        if (dayOfWeek === 6) {
+          attributes.value.push({
+            key: 'available-saterday',
+            content: {
+              class: 'available-symbol',
+              color: 'blue'
+            },
+            dates: {
+              start: formattedDate,
+              end: formattedDate
+            }
+          })
+        } else if (dayOfWeek === 0 || isHoliday === true) {
+          attributes.value.push({
+            key: 'available-sunday-holiday',
+            content: {
+              class: 'available-symbol',
+              color: 'red'
+            },
+            dates: {
+              start: formattedDate,
+              end: formattedDate
+            }
+          })
+        } else {
+          attributes.value.push({
+            key: 'available',
+            content: { class: 'available-symbol' },
+            dates: {
+              start: formattedDate,
+              end: formattedDate
+            }
+          })
+        }
+      }
+    }
+
+  } catch (error) {
+    console.error("Error fetching daily availability:", error)
+  }
+}
+
+// ページが読み込まれた時に利用可能日と時間帯を取得
+onMounted(() => {
+  fetchAvailableDays()
+  fetchAvailableTimes()
+})
+
+// 予約日、時間、人数が変更された時に利用可能時間帯を更新
+watch([visit_date, visit_time, number_of_guests], fetchAvailableTimes)
+
+// 合計価格を計算
 const totalPrice = computed(() => {
     return number_of_guests.value * props.restaurantPrice
 })
@@ -126,6 +304,7 @@ const formattedTotalPrice = computed(() => {
     return new Intl.NumberFormat('ja-JP').format(price)
 })
 
+// 予約処理
 async function submitReservation() {
     const data = {
         visit_date: visit_date.value,
@@ -143,221 +322,6 @@ async function submitReservation() {
         console.error("Error:", error)
     }
 }
-
-const isReservableTime = computed(() => {
-  if (!availableTimes.value) {
-    return false
-  } else {
-    return availableTimes.value.includes(visit_time.value)
-  }
-})
-
-
-const selectBoxWidth = ref("")
-
-onMounted(() => {
-    const elementWidth = document.querySelector(".vc-container").offsetWidth
-    selectBoxWidth.value = `${elementWidth}px`
-})
-
-const timeOptions = (() => {
-  const times = []
-  const startHour = parseInt(props.restaurantStartTime.split(":")[0])
-  const startMinutes = parseInt(props.restaurantStartTime.split(":")[1])
-  let endHour = parseInt(props.restaurantEndTime.split(":")[0])
-  const endMinutes = parseInt(props.restaurantEndTime.split(":")[1])
-
-  if (endHour >= 0 && endHour <= 2) {
-    endHour += 24
-  }
-  endHour -= 2
-  
-  let currentMinute = startMinutes
-  
-  for (let i = startHour; i <= endHour; i++) {
-    if(currentMinute === 60) {
-      currentMinute = 0
-    }
-    while(currentMinute < 60) {
-      times.push(`${i.toString().padStart(2, '0')}:${currentMinute.toString().padStart(2, '0')}`)
-      if (i === endHour && currentMinute >= endMinutes) {
-        break
-      }
-      currentMinute += 30
-    }
-    currentMinute = 0
-  }
-
-  return times
-})()
-
-const visit_time = ref(timeOptions[0])
-const availableTimes = ref(null)
-let cachedVisitDate = null
-
-async function fetchAvailableTimes() {
-    try {
-        function getLocalDate(date) {
-            const year = date.getFullYear()
-            const month = (date.getMonth() + 1).toString().padStart(2, '0')
-            const day = date.getDate().toString().padStart(2, '0')
-            return `${year}-${month}-${day}`
-        }
-
-        const selectedDate = getLocalDate(new Date(visit_date.value ? visit_date.value : cachedVisitDate))
-        const response = await axios.get(`/available-seats`, {
-            params: {
-                visit_date: selectedDate,
-                start_time: props.restaurantStartTime,
-                end_time: props.restaurantEndTime,
-                restaurant_id: props.restaurantId
-            }
-        })
-        cachedVisitDate = selectedDate
-
-        const reservedSeatsData = response.data.reserved_seats
-
-        // fetchAvailableTimes 関数内の該当部分
-        availableTimes.value = timeOptions.filter(time => {
-            const reservedSeats = reservedSeatsData[time] || 0
-            const availableSeats = props.restaurantSeat - reservedSeats - number_of_guests.value
-            return availableSeats >= 0
-        })
-
-    } catch (error) {
-        console.error("Error fetching available seats:", error)
-    }
-}
-
-const dailyAvailability = ref({})
-
-let year = tomorrow.getFullYear()
-let month = tomorrow.getMonth()
-
-month += 2
-if (month > 11) {
-    month -= 12
-    year++
-}
-
-const endOfMonth = new Date(year, month + 1, 1)
-const maxMonth = new Date(year, month + 1, 0)
-
-
-async function fetchAvailableDays() {
-    try {
-        const response = await axios.get(`/available-days`, {
-            params: {
-                start_date: tomorrow,
-                end_date: endOfMonth,
-                start_time: props.restaurantStartTime,
-                end_time: props.restaurantEndTime,
-                restaurant_id: props.restaurantId,
-                restaurant_seat: props.restaurantSeat
-            }
-        })
-
-        dailyAvailability.value = response.data
-
-        const holidays = await fetchJapaneseHolidays(today.getFullYear())
-
-        for (let formattedDate in dailyAvailability.value) {
-          const date = new Date(formattedDate)
-          const dayOfWeek = date.getDay()
-          const isHoliday = holidays.includes(formattedDate)
-          if (dayOfWeek === closedDay) {
-            attributes.value.push({
-              key: 'closed-day',
-              content: { class: 'closed-day-symbol'},
-              dates: {
-                start: formattedDate,
-                end: formattedDate
-              }
-            })
-          } else if (dailyAvailability.value[formattedDate] === true) {
-            if (dayOfWeek === 6) {
-              attributes.value.push({
-                key: 'full-saterday',
-                content: {
-                  class: 'full-booked-symbol',
-                  color: 'blue'
-                },
-                dates: {
-                  start: formattedDate,
-                  end: formattedDate
-                }
-              })
-            } else if (dayOfWeek === 0  || isHoliday === true) {
-              attributes.value.push({
-                key: 'full-sunday-holiday',
-                content: {
-                  class: 'full-booked-symbol',
-                  color: 'red'
-                },
-                dates: {
-                  start: formattedDate,
-                  end: formattedDate
-                }
-              })
-            } else {
-              attributes.value.push({
-                key: 'full',
-                content: { class: 'full-booked-symbol'},
-                dates: {
-                  start: formattedDate,
-                  end: formattedDate
-                }
-              })
-            }
-          } else if (dailyAvailability.value[formattedDate] === false) {
-            if (dayOfWeek === 6) {
-              attributes.value.push({
-                key: 'available-saterday',
-                content: {
-                  class: 'available-symbol',
-                  color: 'blue'
-                },
-                dates: {
-                  start: formattedDate,
-                  end: formattedDate
-                }
-              })
-            } else if (dayOfWeek === 0 || isHoliday === true) {
-              attributes.value.push({
-                key: 'available-sunday-holiday',
-                content: {
-                  class: 'available-symbol',
-                  color: 'red'
-                },
-                dates: {
-                  start: formattedDate,
-                  end: formattedDate
-                }
-              })
-            } else {
-              attributes.value.push({
-              key: 'available',
-              content: { class: 'available-symbol' },
-              dates: {
-                start: formattedDate,
-                end: formattedDate
-              }
-            })
-            }
-          }
-        }
-
-    } catch (error) {
-        console.error("Error fetching daily availability:", error);
-    }
-}
-
-onMounted(() => {
-  fetchAvailableDays()
-  fetchAvailableTimes()
-})
-
-watch([visit_date, visit_time, number_of_guests], fetchAvailableTimes)
 </script>
 
 
