3b6977812d0852cbc1e4f911747f68bb189e434d
yusuke-97 <tigersyuu7@gmail.com>
2023-10-12 18:43:43 +0900

    ポイントチャージ画面の修正

--- src/resources/views/users/charge.blade.php
+++ src/resources/views/users/charge.blade.php
@@ -2,18 +2,23 @@
 
 @section('content')
 <div class="container d-flex justify-content-center mt-3">
-    <h1>ポイントチャージ</h1>
+    <div class="w-50">
+        <h4 class="mt-4 mb-4 sub-title" style="font-weight: bold;">
+            <i class="fas fa-coins"></i>
+            ポイントチャージ
+        </h4>
 
-    <form action="{{ route('charge.point') }}" method="POST">
-        @csrf
+        <form action="{{ route('charge.point') }}" method="POST">
+            @csrf
 
-        <input type="radio" name="point" value="1000"> 1000ポイント<br>
-        <input type="radio" name="point" value="2000"> 2000ポイント<br>
-        <input type="radio" name="point" value="5000"> 5000ポイント<br>
-        <input type="radio" name="point" value="10000"> 10000ポイント<br>
-        <input type="radio" name="point" value="30000"> 30000ポイント<br>
+            <input type="radio" name="point" value="1000" class="mb-3"><strong> 1,000</strong> ポイント<br>
+            <input type="radio" name="point" value="2000" class="mb-3"><strong> 2,000</strong> ポイント<br>
+            <input type="radio" name="point" value="5000" class="mb-3"><strong> 5,000</strong> ポイント<br>
+            <input type="radio" name="point" value="10000" class="mb-3"><strong> 10,000</strong> ポイント<br>
+            <input type="radio" name="point" value="30000" class="mb-3"><strong> 30,000</strong> ポイント<br>
 
-        <button type="submit">ポイントをチャージ</button>
-    </form>
+            <button type="submit" class="btn submit-button mt-3">チャージする</button>
+        </form>
+    </div>
 </div>
 @endsection
