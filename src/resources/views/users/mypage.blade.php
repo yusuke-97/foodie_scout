a54c1a7beee6fcf50e56817cd5d8b262f0b060d2
yusuke-97 <tigersyuu7@gmail.com>
2023-10-12 17:08:26 +0900

    マイページにポイントチャージの追加

--- src/resources/views/users/mypage.blade.php
+++ src/resources/views/users/mypage.blade.php
@@ -57,6 +57,29 @@
             <div class="d-flex justify-content-between">
                 <div class="row">
                     <div class="col-2 d-flex align-items-center">
+                        <i class="fas fa-coins fa-3x"></i>
+                    </div>
+                    <div class="col-9 d-flex align-items-center ms-3 mt-3">
+                        <div class="d-flex flex-column">
+                            <label style="font-weight: bold;">ポイント</label>
+                            <p>ポイントをチャージできます</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex align-items-center">
+                    <a href="{{route('mypage.reservation_history')}}">
+                        <i class="fas fa-chevron-right fa-2x"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="container">
+            <div class="d-flex justify-content-between">
+                <div class="row">
+                    <div class="col-2 d-flex align-items-center">
                         <i class="fas fa-blog fa-3x"></i>
                     </div>
                     <div class="col-9 d-flex align-items-center ms-3 mt-3">
