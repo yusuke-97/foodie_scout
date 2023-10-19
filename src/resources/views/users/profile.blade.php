0ed8338b0a21aba8d94a6070739c7b4a32e28840
yusuke-97 <tigersyuu7@gmail.com>
2023-10-19 12:18:28 +0900

    プロフィール画面の修正

--- src/resources/views/users/profile.blade.php
+++ src/resources/views/users/profile.blade.php
@@ -1,58 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container d-flex justify-content-center mt-3">
-    <!-- デスクトップ用プロフィール -->
-    <div class="row">
-
-        <!-- 左側のカラム：プロフィール画像とユーザー名表示エリア -->
-        <div class="col-4 text-center">
-            @if($user->profile_image)
-            <!-- プロフィール画像の表示 -->
-            <img class="profile-image" src="{{ asset('profile_images/' . $user->profile_image) }}" alt="プロフィール画像">
-            @else
-            <!-- プロフィール画像がない場合のアイコン表示 -->
-            <i class="fas fa-user profile-icon"></i>
-            @endif
-
-            <!-- ユーザー名の表示 -->
-            <p class="user-name mt-3" style="font-weight: bold;">{{ "@" . $user->user_name }}</p>
-        </div>
-
-        <!-- 右側のカラム：ユーザーの詳細情報表示エリア -->
-        <div class="col-8">
-            <div class="d-flex align-items-center justify-content-between">
-
-                <!-- ユーザーの本名表示 -->
-                <p class="mb-0 mr-2" style="font-weight: bold;">{{ $user->name }}</p>
-
-                <medal-color :user-followed="{{ $user->followers->count() }}">
-                </medal-color>
-
+<div class="row justify-content-center">
+    <div class="col-8">
+        <div class="row">
+            <!-- プロフィール画像とユーザー名表示エリア -->
+            <div class="col-4">
+                <div id="profile-image-container">
+                    @if($user->image)
+                    <img class="profile-image" src="{{ asset('/storage/profile_images/' . $user->image) }}" alt="プロフィール画像">
+                    @else
+                    <i class="fas fa-user profile-icon"></i>
+                    @endif
+                </div>
+                <p class="mt-2" style="font-weight: bold;">{{ "@" . $user->user_name }}</p>
             </div>
 
-            <!-- ユーザーのフォロー、フォロワー、投稿数情報 -->
-            <div class="d-flex justify-content-between mt-2">
+            <!-- ユーザーの詳細情報表示エリア -->
+            <div class="col-8">
+                <div class="d-flex align-items-center justify-content-between mb-4">
+                    <h4 class="mb-0 mr-2" style="font-weight: bold;">{{ $user->name }}</h4>
+                    <medal-color :user-followed="{{ $user->followers->count() }}"></medal-color>
+                </div>
+                <div class="d-flex justify-content-between mt-2">
+                    <!-- フォロワー数 -->
+                    <div class="text-center">
+                        <div>フォロワー</div>
+                        <div><strong>{{ $user->followers->count() }}</strong></div>
+                    </div>
 
-                <!-- フォロワー数 -->
-                <p>
-                    フォロワー
-                    <strong>{{ $user->followers->count() }}</strong>
-                    人
-                </p>
+                    <!-- 行ったお店 -->
+                    <div class="text-center">
+                        <div>行ったお店</div>
+                        <div><strong>{{ $user->reservations->count() }}</strong></div>
+                    </div>
 
-                <!-- 来店数 -->
-                <p>
-                    来店数
-                    <strong>{{ $user->reservations->count() }}</strong>
-                </p>
-
-                <!-- 予約数 -->
-                <p>
-                    予約数
-                    <strong>{{ $user->reservations->count() }}</strong>
-                </p>
-
+                    <!-- 予約数 -->
+                    <div class="text-center">
+                        <div>予約数</div>
+                        <div><strong>{{ $user->reservations->count() }}</strong></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
