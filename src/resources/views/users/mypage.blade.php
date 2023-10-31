17dfc3fa09086e8cf314f11af69787b7c057ea97
yusuke-97 <tigersyuu7@gmail.com>
2023-10-31 17:25:47 +0900

    マイページの修正

--- src/resources/views/users/mypage.blade.php
+++ src/resources/views/users/mypage.blade.php
@@ -122,7 +122,7 @@
                     </div>
                 </div>
                 <div class="d-flex align-items-center">
-                    <a href="{{route('mypage.profile')}}">
+                    <a href="{{route('mypage.profile', Auth::user()->id)}}">
                         <i class="fas fa-chevron-right fa-2x"></i>
                     </a>
                 </div>
