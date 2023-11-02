524611569272b3e8f8fa32e6731b47acc05877a6
yusuke-97 <tigersyuu7@gmail.com>
2023-11-02 17:30:36 +0900

    show_followerss.blade.phpの修正

--- src/resources/views/modals/show_followers.blade.php
+++ src/resources/views/modals/show_followers.blade.php
@@ -11,18 +11,30 @@
                     <div class="col-2">
                         <a href="{{ route('mypage.profile', $follower->id) }}" id="small-profile-image-container" style="text-decoration: none;">
                             @if($follower->image)
-                            <img class="small-profile-image" src="{{ asset('/storage/profile_images/' . $follower->image) }}" alt="プロフィール画像">
+                                <img class="small-profile-image" src="{{ asset('/storage/profile_images/' . $follower->image) }}" alt="プロフィール画像">
                             @else
-                            <i class="fas fa-user small-profile-icon" style="color: #000000;"></i>
+                                <i class="fas fa-user small-profile-icon" style="color: #000000;"></i>
                             @endif
                         </a>
                     </div>
-                    <div class="col-9">
-                        <a href="{{ route('mypage.profile', $follower->id) }}" class="d-inline-block" style="text-decoration: none;">
-                            <div style="font-weight: bold; color: #000000;">{{ $follower->name }}</div>
-                            <div style="color: gray; font-size: 12px;">{{ $follower->user_name }}</div>
-                        </a>
-                    </div>
+                    @if(Auth::user()->id === $follower->id)
+                        <div class="col-9">
+                            <a href="{{ route('mypage.profile', $follower->id) }}" class="d-inline-block" style="text-decoration: none;">
+                                <div style="font-weight: bold; color: #000000;">{{ $follower->name }}</div>
+                                <div style="color: gray; font-size: 12px;">{{ $follower->user_name }}</div>
+                            </a>
+                        </div>
+                    @else
+                        <div class="col-5">
+                            <a href="{{ route('mypage.profile', $follower->id) }}" class="d-inline-block" style="text-decoration: none;">
+                                <div style="font-weight: bold; color: #000000;">{{ $follower->name }}</div>
+                                <div style="color: gray; font-size: 12px;">{{ $follower->user_name }}</div>
+                            </a>
+                        </div>
+                        <div class="col-4">
+                            <follow-button :is-following="{{ Auth::user()->isFollowing($follower) ? 'true' : 'false' }}" :following-id="{{ $follower->id }}"></follow-button>
+                        </div>
+                    @endif
                     <div class="col-1">
                         <medal-color :user-followed="{{ $follower->followers->count() }}" class="d-inline-block"></medal-color>
                     </div>
