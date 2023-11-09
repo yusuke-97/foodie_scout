419deeff4b0f49a29b1d5c05d83340849b3604f3
yusuke-97 <tigersyuu7@gmail.com>
2023-11-09 12:04:15 +0900

    サイドバーのレスポンシブデザインの実装

--- src/resources/views/components/sidebar.blade.php
+++ src/resources/views/components/sidebar.blade.php
@@ -3,16 +3,18 @@
 </div>
 <div class="container" style="display: flex; flex-direction: row; justify-content: space-between; flex-wrap: wrap;">
     @foreach ($major_categories as $major_category)
-    <div style="margin-bottom: 20px;">
+    <div class=" col-12 col-md-6 p-4">
         <h3 style="font-weight: bold;">{{ $major_category->name }}</h3>
-        <div style="display: flex; flex-wrap: wrap;">
+        <div class="row">
             @foreach ($categories as $category)
             @if ($category->major_category_id === $major_category->id)
-            <div style="position: relative; margin-right: 10px;">
-                <a href="{{ route('restaurants.index', ['category' => $category->id]) }}" class="restaurant-image-link" style="display: block;">
-                    <img src="{{ asset('img/foodie_' . $category->id . '.jpg') }}" alt="{{ $category->name }}" style="width: 128px; height: 128px; object-fit: cover;" />
-                    <span style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); color: #fff; font-weight: bold; font-size: 16px; white-space: nowrap;">{{ $category->name }}</span>
-                </a>
+            <div class="col">
+                <div class="ratio ratio-1x1" style="position: relative;">
+                    <a href="{{ route('restaurants.index', ['category' => $category->id]) }}" class="restaurant-image-link" style="display: block;">
+                        <img src="{{ asset('img/foodie_' . $category->id . '.jpg') }}" alt="{{ $category->name }}" style="width: 100%; height: 100%; object-fit: cover;" />
+                        <span>{{ $category->name }}</span>
+                    </a>
+                </div>
             </div>
             @endif
             @endforeach
