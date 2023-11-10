af1053d9b6629355043cac131b35ff62c3ff1e46
yusuke-97 <tigersyuu7@gmail.com>
2023-11-10 15:14:36 +0900

    料理ジャンルから探す欄の修正

--- src/resources/views/components/sidebar.blade.php
+++ src/resources/views/components/sidebar.blade.php
@@ -3,22 +3,22 @@
 </div>
 <div class="container" style="display: flex; flex-direction: row; justify-content: space-between; flex-wrap: wrap;">
     @foreach ($major_categories as $major_category)
-    <div class=" col-12 col-lg-6 p-4">
-        <h3 style="font-weight: bold;">{{ $major_category->name }}</h3>
-        <div class="row">
-            @foreach ($categories as $category)
-            @if ($category->major_category_id === $major_category->id)
-            <div class="col">
-                <div class="ratio ratio-1x1" style="position: relative;">
-                    <a href="{{ route('restaurants.index', ['category' => $category->id]) }}" class="restaurant-image-link" style="display: block;">
-                        <img src="{{ asset('img/foodie_' . $category->id . '.jpg') }}" alt="{{ $category->name }}" style="width: 100%; height: 100%; object-fit: cover;" />
-                        <span>{{ $category->name }}</span>
-                    </a>
-                </div>
+        <div class=" col-12 col-lg-6">
+            <h5 style="font-weight: bold; margin-top: 16px;">{{ $major_category->name }}</h5>
+            <div class="row" id="cuisine-major-category">
+                @foreach ($categories as $category)
+                    @if ($category->major_category_id === $major_category->id)
+                        <div class="col" id="cuisine-category">
+                            <div class="ratio ratio-1x1" style="position: relative;">
+                                <a href="{{ route('restaurants.index', ['category' => $category->id]) }}" class="restaurant-image-link" style="display: block;">
+                                    <img src="{{ asset('img/foodie_' . $category->id . '.jpg') }}" alt="{{ $category->name }}" style="width: 100%; height: 100%; object-fit: cover;" />
+                                    <span>{{ $category->name }}</span>
+                                </a>
+                            </div>
+                        </div>
+                    @endif
+                @endforeach
             </div>
-            @endif
-            @endforeach
         </div>
-    </div>
     @endforeach
 </div>
