73d83a3515017b6d317d127daf7b10a24f2b7426
yusuke-97 <tigersyuu7@gmail.com>
2023-11-12 15:59:47 +0900

    検索結果画面の実装

--- src/resources/views/restaurants/index.blade.php
+++ src/resources/views/restaurants/index.blade.php
@@ -1,51 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row justify-content-center" style="margin-top: 30px;">
-    <div class="col-8">
-        @if (isset($restaurants))
-        <h3 style="font-weight: bold; font-size: 24px;">{{ $category->name }}の店舗一覧{{$total_count}}件</h3>
-        @foreach ($restaurants as $restaurant)
-        <hr>
-        <div style="margin-bottom: 20px;">
-            <div class="row">
-                <div class="col-5 col-md-4 col-lg-3">
-                    <div class="ratio ratio-1x1">
-                        <a href="{{ route('restaurants.show', $restaurant) }}" class="restaurant-image-container">
-                            <img src="{{ $restaurant->image }}" class="restaurant-image">
-                        </a>
-                    </div>
-                </div>
-                <div class="col-7 col-md-8 col-lg-9">
-                    <p class="restaurant-name">{{ $restaurant->name }}</p>
-                    <p class="restaurant-info mb-0">
-                        <span>{{ $restaurant->nearest_station }}駅</span>
-                        /
-                        <span style="font-weight: bold;">{{ $category->name }}</span>
-                    </p>
-                    <hr class="mt-1 mb-1">
-                    <p class="mb-0 restaurant-catchphrase">{{ $restaurant->catchphrase }}</p>
-                    <div class="d-flex align-items-center">
-                        <div class="restaurant-average-rating-star">
-                            @for ($i = 1; $i <= 5; $i++) @if ($i <=round($restaurant->average_rating)) <span style="color: #FFA500;">★</span>
-                                @else
-                                <span style="color: #DDDDDD;">★</span>
-                                @endif
-                                @endfor
-                        </div>
-                        <h3 class="restaurant-average-rating-number">{{number_format($restaurant->average_rating, 2) }}</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
-        @endforeach
-        <div class="d-flex justify-content-center">
-            {{ $restaurants->links() }}
-        </div>
-        @else
-        <h3>結果が見つかりませんでした</h3>
-        @endif
-    </div>
-</div>
+    <div class="row justify-content-center" style="margin-top: 30px;">
+        <div class="col-8">
+            @if (isset($restaurants))
+                <h3 style="font-weight: bold; font-size: 24px;">{{ $category->name }}の店舗一覧{{$total_count}}件</h3>
 
+                @foreach ($restaurants as $restaurant)
+                    <hr>
+                    <div style="margin-bottom: 20px;">
+                        <div class="row">
+                            <div class="col-5 col-md-4 col-lg-3">
+                                <div class="ratio ratio-1x1">
+                                    <a href="{{ route('restaurants.show', $restaurant) }}" class="restaurant-image-container">
+                                        <img src="{{ $restaurant->image }}" class="restaurant-image">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-7 col-md-8 col-lg-9">
+                                <p class="restaurant-name">{{ $restaurant->name }}</p>
+                                <p class="restaurant-info mb-0">
+                                    <span>{{ $restaurant->nearest_station }}駅</span>
+                                    /
+                                    <span style="font-weight: bold;">{{ $category->name }}</span>
+                                </p>
+                                <hr class="mt-1 mb-1">
+                                <p class="mb-0 restaurant-catchphrase">{{ $restaurant->catchphrase }}</p>
+                                <div class="d-flex align-items-center">
+                                    <div class="restaurant-average-rating-star">
+                                        @for ($i = 1; $i <= 5; $i++)
+                                            @if ($i <=round($restaurant->average_rating))
+                                                <span style="color: #FFA500;">★</span>
+                                            @else
+                                                <span style="color: #DDDDDD;">★</span>
+                                            @endif
+                                        @endfor
+                                    </div>
+                                    <h3 class="restaurant-average-rating-number">{{number_format($restaurant->average_rating, 2) }}</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+
+                <div class="d-flex justify-content-center">
+                    {{ $restaurants->links() }}
+                </div>
+            @else
+                <h3>結果が見つかりませんでした</h3>
+            @endif
+        </div>
+    </div>
 @endsection
--- src/resources/views/restaurants/search.blade.php
+++ src/resources/views/restaurants/search.blade.php
@@ -1,52 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row justify-content-center" style="margin-top: 30px;">
-    <div class="col-8">
-        @if (isset($results))
-            <h3 style="font-weight: bold; font-size: 24px;">{{ $area }}の{{ $category }}のお店</h3>
-            @foreach ($results as $restaurant)
-            <hr>
-            <div style="margin-bottom: 20px;">
-                <div class="row">
-                    <div class="col-4">
-                        <a href="{{ route('restaurants.show', ['restaurant' => $restaurant['_source']['id']]) }}" style="text-decoration: none;">
-                            <img src="{{ $restaurant['_source']['image'] }}" alt="{{ $restaurant['_source']['name'] }}" style="width: 200px; height: 200px; object-fit: cover;">
-                        </a>
-                    </div>
-                    <div class="col-8">
-                        <a href="{{ route('restaurants.show', ['restaurant' => $restaurant['_source']['id']]) }}" style="text-decoration: none;">
-                            <p style="color: #1E90FF; font-weight: bold; font-size: 24px;">{{ $restaurant['_source']['name'] }}</p>
-                        </a>
-                        <p>
-                            <span>{{ $restaurant['_source']['nearest_station'] }}駅</span>
-                            /
-                            <span style="font-weight: bold;">{{ $category }}</span>
-                        </p>
-                        <hr>
-                        <p class="mb-0">{{ $restaurant['_source']['catchphrase'] }}</p>
-                        <div class="d-flex align-items-center">
-                            <div class="me-3">
-                                @for ($i = 1; $i <= 5; $i++)
-                                    @if ($i <=round($restaurant['_source']['average_rating']))
-                                        <span style="color: #FFA500; font-size: 24px;">★</span>
-                                    @else
-                                        <span style="color: #DDDDDD; font-size: 24px;">★</span>
-                                    @endif
-                                @endfor
+    <div class="row justify-content-center" style="margin-top: 30px;">
+        <div class="col-8">
+            @if (isset($results))
+                <h3 style="font-weight: bold; font-size: 24px;">{{ $area }}の{{ $category }}のお店</h3>
+                
+                @foreach ($results as $restaurant)
+                    <hr>
+                    <div style="margin-bottom: 20px;">
+                        <div class="row">
+                            <div class="col-5 col-md-4 col-lg-3">
+                                <div class="ratio ratio-1x1">
+                                    <a href="{{ route('restaurants.show', ['restaurant' => $restaurant['_source']['id']]) }}" class="restaurant-image-container">
+                                        <img src="{{ $restaurant['_source']['image'] }}" alt="{{ $restaurant['_source']['name'] }}" class="restaurant-image">
+                                    </a>
+                                </div>
                             </div>
-                            <h3 class="mb-0" style="color: red; font-weight: bold; vertical-align: middle;">{{number_format($restaurant['_source']['average_rating'], 2) }}</h3>
+                            <div class="col-7 col-md-8 col-lg-9">
+                                <a href="{{ route('restaurants.show', ['restaurant' => $restaurant['_source']['id']]) }}" style="text-decoration: none;">
+                                    <p class="restaurant-name">{{ $restaurant['_source']['name'] }}</p>
+                                </a>
+                                <p class="restaurant-info mb-0">
+                                    <span>{{ $restaurant['_source']['nearest_station'] }}駅</span>
+                                    /
+                                    <span style="font-weight: bold;">{{ $category }}</span>
+                                </p>
+                                <hr class="mt-1 mb-1">
+                                <p class="mb-0 restaurant-catchphrase">{{ $restaurant['_source']['catchphrase'] }}</p>
+                                <div class="d-flex align-items-center">
+                                    <div class="restaurant-average-rating-star">
+                                        @for ($i = 1; $i <= 5; $i++)
+                                            @if ($i <=round($restaurant['_source']['average_rating']))
+                                                <span style="color: #FFA500;">★</span>
+                                            @else
+                                                <span style="color: #DDDDDD;">★</span>
+                                            @endif
+                                        @endfor
+                                    </div>
+                                    <h3 class="restaurant-average-rating-number">{{number_format($restaurant['_source']['average_rating'], 2) }}</h3>
+                                </div>
+                            </div>
                         </div>
                     </div>
+                @endforeach
+                
+                <div class="d-flex justify-content-center">
+                    {{ $results->links() }}
                 </div>
-            </div>
-            @endforeach
-            <div class="d-flex justify-content-center">
-                {{ $results->links() }}
-            </div>
-        @else
-            <h3>結果が見つかりませんでした</h3>
-        @endif
+            @else
+                <h3>結果が見つかりませんでした</h3>
+            @endif
+        </div>
     </div>
-</div>
 @endsection
