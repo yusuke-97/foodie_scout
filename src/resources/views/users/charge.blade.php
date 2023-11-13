a7b7d5f66570f7e12eb766d1e345c7824b954422
yusuke-97 <tigersyuu7@gmail.com>
2023-11-13 17:42:27 +0900

    ポイントチャージ画面のレスポンシブデザインの実装

--- src/resources/views/users/charge.blade.php
+++ src/resources/views/users/charge.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container d-flex justify-content-center mt-3">
-    <div class="w-50">
-        <point-charge :is-registered="'{{ Auth::user()->token }}'"></point-charge>
+    <div class="container row d-flex justify-content-center mt-3">
+        <div class="col-12 col-md-6">
+            <point-charge :is-registered="'{{ Auth::user()->token }}'"></point-charge>
+        </div>
     </div>
-</div>
 @endsection
