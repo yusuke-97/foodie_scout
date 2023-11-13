462de024d13f28f098f7859e30d36b76822e9845
yusuke-97 <tigersyuu7@gmail.com>
2023-11-13 16:42:28 +0900

    タイムライン画面のレスポンシブデザインの実装

--- src/resources/views/reviews/index.blade.php
+++ src/resources/views/reviews/index.blade.php
@@ -3,24 +3,26 @@
 @section('content')
 <div class="container" style="margin-top: 30px;">
     <div class="row justify-content-center">
-        <div class="col-12 col-md-5">
+        <div class="col-12 col-md-8 col-lg-6">
             @foreach ($reviews as $category_id => $reviews_category)
             @foreach ($reviews_category as $review)
             <div class="card mb-5">
                 <div class="card-header">
                     <div class="row align-items-center">
                         <div class="col-2">
-                            <a href="{{ route('mypage.profile', $review->user->id) }}" class="small-profile-image-container" style="text-decoration: none;">
-                                @if($review->user->image)
-                                <img class="small-profile-image" src="{{ asset('/storage/profile_images/' . $review->user->image) }}" alt="プロフィール画像">
-                                @else
-                                <i class="fas fa-user small-profile-icon" style="color: #000000;"></i>
-                                @endif
-                            </a>
+                            <div class="ratio ratio-1x1">
+                                <a href="{{ route('mypage.profile', $review->user->id) }}" class="small-profile-image-container" style="text-decoration: none;">
+                                    @if($review->user->image)
+                                        <img class="small-profile-image" src="{{ asset('/storage/profile_images/' . $review->user->image) }}" alt="プロフィール画像">
+                                    @else
+                                        <i class="fas fa-user small-profile-icon" style="color: #000000;"></i>
+                                    @endif
+                                </a>
+                            </div>
                         </div>
                         <div class="col-10">
                             <a href="{{ route('mypage.profile', $review->user->id) }}" class="d-inline-block" style="text-decoration: none;">
-                                <span style="font-weight: bold; color: #000000;" class="me-3">{{ $review->user->name }}</span>
+                                <span style="font-weight: bold; color: #000000;" class="user-name me-3">{{ $review->user->name }}</span>
                             </a>
                             <medal-color :user-followed="{{ $review->user->followers->count() }}" class="d-inline-block"></medal-color>
                         </div>
@@ -36,21 +38,21 @@
                         <a href="{{ route('restaurants.show', $review->restaurant) }}" style="text-decoration: none;">
                             <p style="color: #1E90FF; font-weight: bold; font-size: 20px;" class="mb-0 mt-2">{{ $review->restaurant->name }}</p>
                         </a>
-                        <p style="font-size: 12px;" class="mb-0">
+                        <p class="restaurant-information mb-0">
                             <span>{{ $review->restaurant->nearest_station }}駅</span>
                             /
                             <span style="font-weight: bold;">{{ $review->restaurant->category->name }}</span>
                         </p>
                         <div class="d-flex align-items-center mb-3">
-                            <div class="me-3">
+                            <div class="restaurant-average-rating-star">
                                 @for ($i = 1; $i <= 5; $i++) @if ($i <=round($review->restaurant->average_rating))
-                                    <span style="color: #FFA500; font-size: 16px;">★</span>
+                                    <span style="color: #FFA500;">★</span>
                                     @else
-                                    <span style="color: #DDDDDD; font-size: 16px;">★</span>
+                                    <span style="color: #DDDDDD;">★</span>
                                     @endif
                                     @endfor
                             </div>
-                            <h3 class="mb-0" style="color: red; font-weight: bold; vertical-align: middle; font-size: 16px;">{{number_format($review->restaurant->average_rating, 2) }}</h3>
+                            <h3 class="restaurant-average-rating-number">{{number_format($review->restaurant->average_rating, 2) }}</h3>
                         </div>
                         @if($review->score === 5)
                         <div>
