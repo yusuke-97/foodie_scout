d0c3d9b28921ac4dec0a58d858c42ac3535ab4bc
yusuke-97 <tigersyuu7@gmail.com>
2023-11-13 16:45:42 +0900

    ユーザー編集画面のレスポンシブデザインの実装

--- src/resources/views/users/edit.blade.php
+++ src/resources/views/users/edit.blade.php
@@ -1,15 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <span>
-                <a href="{{ route('mypage') }}">マイページ</a> > 会員情報の編集
-            </span>
-
-            <user-edit :csrf="'{{ csrf_token() }}'" :user-image="'{{ Auth::user()->image }}'" :full-name="'{{ Auth::user()->name }}'" :user-name="'{{ Auth::user()->user_name }}'" :user-email="'{{ Auth::user()->email }}'" :user-phone-number="'{{ Auth::user()->phone_number }}'"></user-edit>
+    <div class="container">
+        <div class="row justify-content-center" style="margin-top: 30px;">
+            <div class="col-12 col-md-6">
+                <user-edit :csrf="'{{ csrf_token() }}'" :user-image="'{{ Auth::user()->image }}'" :full-name="'{{ Auth::user()->name }}'" :user-name="'{{ Auth::user()->user_name }}'" :user-email="'{{ Auth::user()->email }}'" :user-phone-number="'{{ Auth::user()->phone_number }}'"></user-edit>
+            </div>
         </div>
     </div>
-</div>
 @endsection
