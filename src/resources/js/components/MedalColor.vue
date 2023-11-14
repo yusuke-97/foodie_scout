c116bea5f83534c6aa25d017154bced926f3ea37
yusuke-97 <tigersyuu7@gmail.com>
2023-11-14 14:39:31 +0900

    MedalColor.vueの修正

--- src/resources/js/components/MedalColor.vue
+++ src/resources/js/components/MedalColor.vue
@@ -7,27 +7,27 @@ const props = defineProps({
 })
 
 const medalColor = computed(() => {
-    if (props.isFollowed >= 100) return 'medal-color-platinum';
-    if (props.isFollowed >= 50) return 'medal-color-gold';
-    if (props.isFollowed >= 30) return 'medal-color-silver';
-    if (props.isFollowed >= 10) return 'medal-color-bronze';
-    return 'medal-color-green';
-});
+    if (props.isFollowed >= 100) return 'medal-color-platinum'
+    if (props.isFollowed >= 50) return 'medal-color-gold'
+    if (props.isFollowed >= 30) return 'medal-color-silver'
+    if (props.isFollowed >= 10) return 'medal-color-bronze'
+    return 'medal-color-green'
+})
 
 const medalColorUserFollowed = computed(() => {
-    if (props.userFollowed >= 100) return 'medal-color-platinum';
-    if (props.userFollowed >= 50) return 'medal-color-gold';
-    if (props.userFollowed >= 30) return 'medal-color-silver';
-    if (props.userFollowed >= 10) return 'medal-color-bronze';
-    return 'medal-color-green';
-});
+    if (props.userFollowed >= 100) return 'medal-color-platinum'
+    if (props.userFollowed >= 50) return 'medal-color-gold'
+    if (props.userFollowed >= 30) return 'medal-color-silver'
+    if (props.userFollowed >= 10) return 'medal-color-bronze'
+    return 'medal-color-green'
+})
 
 const isPlatinum = computed(() => {
     if (medalColor.value === 'medal-color-platinum' || medalColorUserFollowed.value === 'medal-color-platinum') {
-        return 'medal-icon';
+        return 'medal-icon'
     }
-    return '';
-});
+    return ''
+})
 </script>
 
 <template>
