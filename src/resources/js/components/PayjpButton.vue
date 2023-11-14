0ccc4926dba488a2335c8de88a2b769376cf71ac
yusuke-97 <tigersyuu7@gmail.com>
2023-11-14 14:42:47 +0900

    PayjpButton.vueの修正

--- src/resources/js/components/PayjpButton.vue
+++ src/resources/js/components/PayjpButton.vue
@@ -4,36 +4,36 @@ import { ref, onMounted } from 'vue'
 
 const script = ref(null)
 const props = defineProps({
-  cardExists: {
-    type: Boolean,
-    required: true
-  },
-  apiKey: {
-    type: String,
-    required: true
-  }
+  	cardExists: {
+    	type: Boolean,
+    	required: true
+  	},
+  	apiKey: {
+    	type: String,
+    	required: true
+  	}
 })
 
 onMounted(() => {
-  const scriptSource = 'https://checkout.pay.jp/'
-  const buttonText = props.cardExists ? 'カードを更新する' : 'カードを登録する'
+	const scriptSource = 'https://checkout.pay.jp/'
+	const buttonText = props.cardExists ? 'カードを更新する' : 'カードを登録する'
 
-  const scriptTag = `
-    <script type="text/javascript" 
-            src="${scriptSource}" 
-            class="payjp-button" 
-            data-key="${props.apiKey}" 
-            data-on-created="onCreated" 
-            data-text="${buttonText}" 
-            data-submit-text="${buttonText}">
-    <\/script>`
+	const scriptTag = `
+		<script type="text/javascript" 
+				src="${scriptSource}" 
+				class="payjp-button" 
+				data-key="${props.apiKey}" 
+				data-on-created="onCreated" 
+				data-text="${buttonText}" 
+				data-submit-text="${buttonText}">
+		<\/script>`
 
-  postscribe(script.value, scriptTag)
+	postscribe(script.value, scriptTag)
 })
 </script>
 
 
 
 <template>
-  <div ref="script"></div>
+  	<div ref="script"></div>
 </template>
