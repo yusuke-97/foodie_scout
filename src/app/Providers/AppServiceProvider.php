7d710caf9c654fb898365ed038aaaff647d3ba42
yusuke-97 <tigersyuu7@gmail.com>
2023-11-14 19:56:07 +0900

    Added heroku.yml

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +23,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+        if (App::environment(['production'])) {
+            URL::forceScheme('https');
+        }
     }
 }
