da0a4151a38e5805558fdb6b5aaf8cf02510fdb2
yusuke-97 <tigersyuu7@gmail.com>
2023-11-14 14:32:30 +0900

    FavoriteButton.vueの修正

--- src/resources/js/components/FavoriteButton.vue
+++ src/resources/js/components/FavoriteButton.vue
@@ -3,28 +3,29 @@ import { ref } from 'vue'
 import axios from 'axios'
 
 const props = defineProps({
-  isFavorited: Boolean,
-  restaurantId: Number
+  	isFavorited: Boolean,
+  	restaurantId: Number
 })
 
-const localIsFavorited = ref(props.isFavorited);
+const localIsFavorited = ref(props.isFavorited)
 
 const toggleFavorite = async () => {
-  try {
-    let response;
-    if (localIsFavorited.value) {
-      // お気に入り済みの場合はお気に入りを解除する
-      response = await axios.delete(`/restaurants/${props.restaurantId}/favorite`);
-    } else {
-      // まだお気に入りではない場合はお気に入りに追加する
-      response = await axios.post(`/restaurants/${props.restaurantId}/favorite`);
-    }
-    if (response.data.status === 'added' || response.data.status === 'removed') {
-      localIsFavorited.value = !localIsFavorited.value;
-    }
-  } catch (error) {
-    console.error("Error:", error);
-  }
+	try {
+		let response
+
+		if (localIsFavorited.value) {
+			// お気に入り済みの場合はお気に入りを解除する
+			response = await axios.delete(`/restaurants/${props.restaurantId}/favorite`)
+		} else {
+			// まだお気に入りではない場合はお気に入りに追加する
+			response = await axios.post(`/restaurants/${props.restaurantId}/favorite`)
+		}
+		if (response.data.status === 'added' || response.data.status === 'removed') {
+			localIsFavorited.value = !localIsFavorited.value
+		}
+	} 	catch (error) {
+			console.error("Error:", error)
+	}
 }
 </script>
 
